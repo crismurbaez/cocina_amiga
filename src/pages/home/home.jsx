9dc3c2051e5d8eaad1b6a84799a248be5f34869b
Cristina <clmbjcm@gmail.com>
2024-02-02 20:54:59 -0300

    home finish

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -220,15 +220,17 @@ const Home = () => {
             </section>
             {/* ---------section our team ------------------ */}
             <section className={s.section_team}>
-                <div className={s.titulo_team}>Nuestro equipo</div>
-                <div className={s.body_team}>
-                    <div className={s.info_team} ><img className={s.img_team} src="./images/team/agustin_rosales.jpg" alt="Agustín Rosales" />
-                        <p className={s.name_team}>Agustín Rosales</p>
-                        <p className={s.detail_team}>Chef</p>
-                    </div>
-                    <div className={s.info_team}><img className={s.img_team} src="./images/team/celeste_soria.jpg" alt="Celeste Soria" />
-                        <p className={s.name_team}>Celeste Soria</p>
-                        <p className={s.detail_team}>Lic. en Turismo</p>
+                <div className={s.container_team}>
+                    <div className={s.titulo_team}>Nuestro equipo</div>
+                    <div className={s.body_team}>
+                        <div className={s.info_team} ><img className={s.img_team} src="./images/team/agustin_rosales.jpg" alt="Agustín Rosales" />
+                            <p className={s.name_team}>Agustín Rosales</p>
+                            <p className={s.detail_team}>Chef</p>
+                        </div>
+                        <div className={s.info_team}><img className={s.img_team} src="./images/team/celeste_soria.jpg" alt="Celeste Soria" />
+                            <p className={s.name_team}>Celeste Soria</p>
+                            <p className={s.detail_team}>Lic. en Turismo</p>
+                        </div>
                     </div>
                 </div>
             </section>
