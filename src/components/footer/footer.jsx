24fc07b183ea8927f033af0b2dd84f0d4739b79a
Cristina <clmbjcm@gmail.com>
2024-02-02 21:29:23 -0300

    add links contacts

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import s from './footer.module.css';
 
 
@@ -8,20 +9,32 @@ const Footer = () => {
             <div className={s.container_footer}>
                 <div className={s.contact_footer}>Podés encontrar a cocina amiga en:</div>
                 <div className={s.container_contact}>
-                    <img className={s.instagram} src="./images/svg/instagram.svg" alt="" />
-                    <img className={s.whatsapp} src="./images/svg/whatsapp.svg" alt="" />
+                    <Link to='https://www.instagram.com/tucocinaamiga/' target='blank'>
+                        <img className={s.instagram} src="./images/svg/instagram.svg" alt="" />
+                    </Link>
+                    <Link to='https://web.whatsapp.com/' target='blank'>
+                        <img className={s.whatsapp} src="./images/svg/whatsapp.svg" alt="" />
+                    </Link>
                 </div>
                 <div className={s.contact_team}>
                     <div className={s.one_team}> Agustín Rosales
                         <div className={s.container_team}>
-                            <img className={s.instagram} src="./images/svg/instagram.svg" alt="" />
-                            <img className={s.whatsapp} src="./images/svg/whatsapp.svg" alt="" />
+                            <Link to='https://www.instagram.com/agustin.rosales99/' target='blank'>
+                                <img className={s.instagram} src="./images/svg/instagram.svg" alt="" />
+                            </Link>
+                            <Link to='https://web.whatsapp.com/' target='blank'>
+                                <img className={s.whatsapp} src="./images/svg/whatsapp.svg" alt="" />
+                            </Link>
                         </div>
                     </div>
                     <div className={s.two_team}>Celeste Soria
                         <div className={s.container_team}>
-                            <img className={s.instagram} src="./images/svg/instagram.svg" alt="" />
-                            <img className={s.whatsapp} src="./images/svg/whatsapp.svg" alt="" />
+                            <Link to='https://www.instagram.com/ce.soria/' target='blank'>
+                                <img className={s.instagram} src="./images/svg/instagram.svg" alt="" />
+                            </Link>
+                            <Link to='https://web.whatsapp.com/' target='blank'>
+                                <img className={s.whatsapp} src="./images/svg/whatsapp.svg" alt="" />
+                            </Link>
                         </div>
 
                     </div>
