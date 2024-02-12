1ab61a158d8d80c6a3a1996e8d29d048aa704c00
Cristina <clmbjcm@gmail.com>
2024-02-11 21:01:47 -0300

    form init

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -4,7 +4,47 @@ import s from './contact.module.css';
 const Contact = () => {
     return (
         <section className={s.contact}>
-            Proximamente formulario de contacto
+            <div className={s.decoration_background}>
+                <div className={s.intro}>
+                    Escríbenos, nos encantaría recibir tu consulta y comentarios.
+                </div>
+            </div>
+            <form
+                name="contact"
+                autoComplete="on"
+                acceptCharset="utf-8"
+                className={s.form}
+            >
+                <label className={s.label} htmlFor="name">Nombre</label>
+                <input
+                    className={s.input}
+                    type="text"
+                    name="name"
+                    placeholder="María Smith" />
+                <label className={s.label} htmlFor="asunto">Asunto</label>
+                <input
+                    className={s.input}
+                    type="text"
+                    name="asunto"
+                    placeholder="Asunto"
+                />
+                <label className={s.label} htmlFor="email">Email</label>
+                <input
+                    className={s.input}
+                    type="text"
+                    name="email"
+                    placeholder="maria@mail.com"
+                />
+                <label className={s.label} htmlFor="detail">Detalle</label>
+                <input
+                    className={s.input}
+                    type="textarea"
+                    name="detail"
+                    placeholder="Escribe todo lo que quieras contarnos, sugerirnos, y comentarnos..."
+                />
+                <input className={s.submit} type="submit" value='Enviar' />
+
+            </form>
         </section>
     )
 
